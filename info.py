699ec6d57ad3a87dbc19ee85d45adb6b65329f2a
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-02-16 19:38:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ import re
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, True):
+def is_enabled(value, 1):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
     elif value.lower() in ["false", "no", "0", "disable", "n"]:
