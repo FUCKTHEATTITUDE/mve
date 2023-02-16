e54fefa6ec4f00feeb64949a2d46180e05ba1c53
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-02-16 19:42:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ import re
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, y):
+def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
     elif value.lower() in ["false", "no", "0", "disable", "n"]:
